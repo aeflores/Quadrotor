0715ca219fb6c574062ae972a0455b8ab687c1f0
aeflores <antoniofloresmontoya@gmail.com>
2019-08-06 22:15:57 -0400

    Initial program as developed by kike

---
+++ Acelerometro.ino
@@ -0,0 +1,241 @@
+int status=-1;
+//Vcc-->3.3V
+//Gnd-->Gnd
+//SDA--> A4
+//SCL--> A5
+
+
+void IMU_ini(){
+    while(status<0){
+    status = IMU.begin();
+    if (status < 0) {
+    Serial.println("IMU initialization unsuccessful");
+    Serial.println("Check IMU wiring or try cycling power");
+    Serial.print("Status: ");
+    Serial.println(status);
+    }
+    else{
+      Serial.println("IMU initialization successful");
+    }
+    }
+}
+
+void IMU_default_cal(){
+
+  //Magnetometer settings
+    // X-axis bias and scale factor
+    float hxb = -4.257; // mag bias in uT
+    float hxs = 0.9838756; // mag scale factor
+    IMU.setMagCalX(hxb,hxs);
+    //Y-axis bias and scale factor
+    float hyb = 27.517288; // mag bias in uT
+    float hys = 1.0328; // mag scale factor
+    IMU.setMagCalY(hyb,hys);
+    // Z-axis bias and scale factor
+    float hzb = -2.302750; // mag bias in uT
+    float hzs = 1.0; // mag scale factor
+    IMU.setMagCalZ(hzb,hzs);
+    
+  //Gyroscope settings
+    // X-axis bias
+    float gxb = 0.006546; // gyro bias 
+    IMU.setGyroBiasX_rads(gxb);
+    // Y-axis bias
+    float gyb = 0.0267601; // gyro bias of 0.001 rad/s
+    IMU.setGyroBiasY_rads(gyb);
+    // Z-axis bias
+    float gzb = -0.012746; // gyro bias of 0.001 rad/s
+    IMU.setGyroBiasZ_rads(gzb);  
+}
+
+void IMU_settings(){
+  //Low Pass Filter configuration
+  //Digital Low Pass Filter (DLPF) bandwidth. 
+  //By default, if this function is not called, a DLPF bandwidth of 184 Hz is used. The following DLPF bandwidths are supported:
+  status = IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_10HZ);
+  
+  //This function sets the accelerometer full scale range to the given value. 
+  //By default, if this function is not called, a full scale range of +/- 16 g will be used. 
+  //The enumerated accelerometer full scale ranges are: +/- 2g,+/- 4g,+/- 8g,+/- 16g,
+  status = IMU.setAccelRange(MPU9250::ACCEL_RANGE_16G);
+  
+  //This function sets the gyroscope full scale range to the given value. 
+  //By default, if this function is not called, a full scale range of +/- 2000 deg/s will be used. 
+  //The enumerated gyroscope full scale ranges are: +/- 250deg/s,+/- 500deg/s,+/- 1000deg/s,+/- 2000deg/s,
+  status = IMU.setGyroRange(MPU9250::GYRO_RANGE_2000DPS);
+  
+}
+
+void Magnetometro_cal(){
+    status=-1;
+    while(status<0){
+    Serial.println("IMU Magnetometer calibration starting in 2 seconds, please move de IMU");
+    delay(2000);
+    status = IMU.calibrateMag();
+    if (status < 0) {
+    Serial.println("IMU calibration unsuccessful");
+    Serial.print("Status: ");
+    Serial.println(status);
+    }
+    else {
+    Serial.println("IMU calibration successful");
+    float hxb;
+    hxb = IMU.getMagBiasX_uT();
+    float hxs;
+    hxs = IMU.getMagScaleFactorX();
+    float hyb;
+    hyb = IMU.getMagBiasY_uT();
+    float hys;
+    hys = IMU.getMagScaleFactorY();
+    float hzb;
+    hzb = IMU.getMagBiasZ_uT();
+    float hzs;
+    hzs = IMU.getMagScaleFactorZ();
+    Serial.print("MagBiasX: ");  
+    Serial.print(hxb,6);
+    Serial.print("  ");
+    Serial.print("MagSFX: ");  
+    Serial.print(hxs,6);
+    Serial.print("  ");
+
+    Serial.print("MagBiasY: ");  
+    Serial.print(hyb,6);
+    Serial.print("  ");
+    Serial.print("MagSFY: ");  
+    Serial.print(hys,6);
+    Serial.print("  "); 
+
+    Serial.print("MagBiasZ: ");  
+    Serial.print(hzb,6);
+    Serial.print("  ");
+    Serial.print("MagSFZ: ");  
+    Serial.println(hzs,6); 
+    }
+    }
+}
+
+void Acelerometro_cal(){
+    status=-1;
+    while(status<0){
+    Serial.println("IMU Accelerometer calibration starting in 2 seconds, do not move the IMU");
+    delay(2000);
+    status = IMU.calibrateAccel();
+    if (status < 0) {
+    Serial.println("IMU calibration unsuccessful");
+    Serial.print("Status: ");
+    Serial.println(status);
+    }
+    else {
+    Serial.println("IMU calibration successful");
+    float axb;
+    axb = IMU.getAccelBiasX_mss();
+    float axs;
+    axs = IMU.getAccelScaleFactorX();
+    
+    float ayb;
+    ayb = IMU.getAccelBiasY_mss();
+    float ays;
+    ays = IMU.getAccelScaleFactorY();    
+    float azb;
+    azb = IMU.getAccelBiasZ_mss();
+    float azs;
+    azs = IMU.getAccelScaleFactorZ();
+    Serial.print("AccBiasX: ");  
+    Serial.print(axb,6);
+    Serial.print("  ");
+    Serial.print("AccSFX: ");  
+    Serial.print(axs,6);
+    Serial.print("  ");
+
+    Serial.print("AccBiasY: ");  
+    Serial.print(ayb,6);
+    Serial.print("  ");
+    Serial.print("AccSFY: ");  
+    Serial.print(ays,6);
+    Serial.print("  ");
+
+    Serial.print("AccBiasZ: ");  
+    Serial.print(azb,6);
+    Serial.print("  ");
+    Serial.print("AccSFZ: ");  
+    Serial.print(azs,6);
+    Serial.println("  ");
+    }
+    }
+}
+
+void Gyroscope_cal(){
+
+  status=-1;
+    while(status<0){
+    Serial.println("IMU Gyroscope calibration starting in 2 seconds, do not move the IMU");
+    delay(2000);
+    status = IMU.calibrateGyro();;
+    if (status < 0) {
+    Serial.println("IMU calibration unsuccessful");
+    Serial.print("Status: ");
+    Serial.println(status);
+    }
+    else {
+    Serial.println("IMU calibration successful");
+    float gxb;
+    gxb = IMU.getGyroBiasX_rads();
+    float gyb;
+    gyb = IMU.getGyroBiasY_rads();
+    float gzb;
+    gzb = IMU.getGyroBiasZ_rads();
+    Serial.print("GyroBiasX: ");  
+    Serial.print(gxb,6);
+    Serial.print("  ");
+    Serial.print("GyroBiasY: ");  
+    Serial.print(gyb,6);
+    Serial.print("  ");
+    Serial.print("GyroBiasZ: ");  
+    Serial.print(gzb,6);
+    Serial.println("  ");
+    }
+    }
+    
+ }
+
+float* get_raw_val(){
+  static float Acc_val[9];
+  // Read the sensor
+  IMU.readSensor();
+  //Get Acceleration
+  float  Acc_x=IMU.getAccelX_mss();
+  float  Acc_y=IMU.getAccelY_mss();
+  float  Acc_z=IMU.getAccelZ_mss();
+  //Get angular velocities
+  float  Gyro_x=IMU.getGyroX_rads();
+  float  Gyro_y=IMU.getGyroY_rads();
+  float  Gyro_z=IMU.getGyroZ_rads();
+  // Get magnetic field components in the body reference
+  float  Mag_x=IMU.getMagX_uT();
+  float  Mag_y=IMU.getMagY_uT();
+  float  Mag_z=IMU.getMagZ_uT();
+  Acc_val[0]=Acc_x;
+  Acc_val[1]=Acc_y;
+  Acc_val[2]=Acc_z;
+  Acc_val[3]=Gyro_x;
+  Acc_val[4]=Gyro_y;
+  Acc_val[5]=Gyro_z;
+  Acc_val[6]=Mag_x;
+  Acc_val[7]=Mag_y;
+  Acc_val[8]=Mag_z;
+  return Acc_val;  
+//  Serial.print("Temperature in C: ");
+//  Serial.println(IMU.getTemperature_C(),6);
+//  Serial.println();
+}
+
+
+  //MagBiasX: -7.138206  MagSFX: 1.079588  MagBiasY: 18.255805  MagSFY: 0.946956  MagBiasZ: 6.438561  MagSFZ: 0.982602
+  //MagBiasX: 13.060284  MagSFX: 1.174667  MagBiasY: 14.025960  MagSFY: 1.094046  MagBiasZ: 18.988445  MagSFZ: 0.809942
+  //MagBiasX: -4.390320  MagSFX: 0.984479  MagBiasY: 23.044439  MagSFY: 1.076520  MagBiasZ: -2.302750  MagSFZ: 0.947584
+  //MagBiasX: -3.515854  MagSFX: 0.936396  MagBiasY: 27.973655  MagSFY: 1.062817  MagBiasZ: 5.071450  MagSFZ: 1.008898
+  //MagBiasX: -4.876480  MagSFX: 1.030752  MagBiasY: 27.517288  MagSFY: 0.959087  MagBiasZ: 3.004229  MagSFZ: 1.012990
+  //MagBiasX: 10.368086  MagSFX: 1.615734  MagBiasY: 32.904544  MagSFY: 0.650060  MagBiasZ: 19.129512  MagSFZ: 1.186567
+
+
+
