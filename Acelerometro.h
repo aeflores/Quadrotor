dd4ccc7331b455c2bbfa784f35b71ffb81e32834
aeflores <antoniofloresmontoya@gmail.com>
2019-08-13 22:57:42 -0400

    move methods to public

--- Acelerometro.h
+++ Acelerometro.h
@@ -15,14 +15,13 @@ private:
   // an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
   MPU9250 IMU;
 
-  void magnetometro_cal();
-  void acelerometro_cal();
-  void gyroscope_cal();
-
 public:
   Acelerometro(TwoWire Wire): IMU(Wire,0x68){};
   void initialize();
   void default_cal();
+  void magnetometro_cal();
+  void acelerometro_cal();
+  void gyroscope_cal();
   void settings();
   float *get_raw_val();
 };
