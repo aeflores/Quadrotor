b662522a388e9c3908ec8e438fc3a9a4c0a919f2
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2021-04-17 19:27:20 -0400

    cleaning up

--- Acc/Quadrotor/Acelerometro.h
+++
@@ -1,8 +0,0 @@
-void IMU_ini();
-void IMU_settings();
-void Magnetometro_cal();
-void Acelerometro_cal();
-void Gyroscope_cal();
-void IMU_default_cal();
-float* get_raw_val();
-
--- Acc_Radio/Quadrotor/Triad.h
+++
@@ -1,5 +0,0 @@
-float *get_ypr_triad(float Acc_raw_val[]);
-float *Integrator(float Acc_raw_val[], float yawpitchroll[]);
-float *filter(float Acc_raw_val[], float yawpitchroll_triad[],
-              float yawpitchroll_int[]);
-
